e64893b5c9cc90a6b0cdc9fdf434f2e0b9cb2ca5
Rogerio-kayronny-sales <108687095+Kayronny123@users.noreply.github.com>
2022-12-15 16:38:29 -0300

    Incluindo objetos dos produtos

---
+++ components/ObjetosProdutos.js
@@ -0,0 +1,40 @@
+import Imagem1 from "./Imagens/arma-portais.jpg";
+import Imagem2 from "./Imagens/bateria-microverso.jpeg";
+import Imagem3 from "./Imagens/caixa-meeseks.jfif";
+import Imagem4 from "./Imagens/espaconave-rick.jpg";
+import Imagem5 from "./Imagens/robo-passa-manteiga.png";
+
+const todosProdutos = () => [
+  {
+    id: 1,
+    produto: "Arma de Portais",
+    preco: 6000,
+    imagem: Imagem1
+  },
+  {
+    id: 2,
+    produto: "Bateria de Microverso",
+    preco: 4000,
+    imagem: Imagem2
+  },
+  {
+    id: 3,
+    produto: "Caixa de Meeseks",
+    preco: 2000,
+    imagem: Imagem3
+  },
+  {
+    id: 4,
+    produto: "Espaçonave",
+    preco: 9000,
+    imagem: Imagem4
+  },
+  {
+    id: 5,
+    produto: "Robozinha passa manteiga",
+    preco: 1000,
+    imagem: Imagem5
+  }
+];
+
+export default todosProdutos;
